0a9821786e27009be2d302b28fde35cc26e75265
Niket Patel <patelniket@users.noreply.github.com>
2014-11-24 16:26:11 -0500

    Project to demonstrate bean definition inheritance
    This project shows how we can use parent bean to inherit properties in
    child bean and how we can also override those properties if needed.

---
+++ BeanDefinitionInheritance/src/me/niketpatel/MainApp.java
@@ -0,0 +1,24 @@
+package me.niketpatel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld objA = (HelloWorld) context.getBean("helloWorld"); 
+		
+		objA.getMessage1();
+		objA.getMessage2();
+		
+		HelloUSA objB = (HelloUSA) context.getBean("helloUSA");
+		
+		objB.getMessage1();
+	    objB.getMessage2();
+	    objB.getMessage3();
+
+	}
+
+}
